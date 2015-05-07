fab1dfd654a622f6ff578f553dc1125207dda746
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-07 10:46:18 +0900

    make full test code for DateCalculator

--- TestDateCaculator.rb
+++ TestDateCaculator.rb
@@ -8,11 +8,52 @@ class TestDateCaculator < Minitest::Test
     return calc.leap_year?
   end
 
+  def add_leap_year(days, year)
+    calc = DateCalculator.new(days)
+    calc.year = year
+    calc.add_leap_year
+    return calc.year
+  end
+
+  def add_regular_year(days, year)
+    calc = DateCalculator.new(days)
+    calc.year = year
+    calc.add_regular_year
+    return calc.year
+  end
+
   def test_leapyear
     assert is_leapyear(2004)
 
     assert not(is_leapyear(1900))
 
     assert is_leapyear(2000)
+  end
+
+  def test_add_leap_year
+    assert_equal 2008, add_leap_year(255, 2008)
+    assert_equal 2009, add_leap_year(400, 2008)
+    assert_equal 2009, add_leap_year(366, 2008)
+  end
+
+  def test_add_regular_year
+    assert_equal 2009, add_regular_year(255, 2009)
+    assert_equal 2010, add_regular_year(400, 2009)
+    assert_equal 2010, add_regular_year(365, 2009)
+  end
+
+  def convert(days, year)
+    calc = DateCalculator.new(days)
+    calc.year = year
+    return calc.convert
+  end
+
+  def test_convert
+    assert_equal 2009, convert(364, 2009)
+    assert_equal 2008, convert(365, 2008)
+    assert_equal 2010, convert(365, 2009)
+    assert_equal 2009, convert(366, 2008)
+    assert_equal 2010, convert(366, 2009)
+    assert_equal 2009, convert(367, 2008)
   end
 end
